451fcc8f7c24cd03578e03e18adc1a5aa9a74ba1
Abdus Salam <iamabdus@gmail.com>
2016-04-07 13:41:07 +0600

    royal 1.2.1
    Linked all pages

--- royal/1.2/js/custom.js
+++ royal/1.2/js/custom.js
@@ -104,6 +104,45 @@ jQuery(document).ready(function(){
 		direction: "horizontal",
 		reverse: false, 
 	});
+
+	(function() {
+ 
+	  // store the slider in a local variable
+	  var $window = $(window),
+	      flexslider = { vars:{} };
+	 
+	  // tiny helper function to add breakpoints
+	  function getGridSize() {
+	    return (window.innerWidth < 600) ? 2 :
+		      (window.innerWidth < 800) ? 3 :
+		      (window.innerWidth < 900) ? 4 : 5;
+	  }
+	 
+	  $(function() {
+	    SyntaxHighlighter.all();
+	  });
+	 
+	  $window.load(function() {
+	    $('.flexslider').flexslider({
+	      animation: "slide",
+	      animationLoop: false,
+	      itemWidth: 215,
+	      itemMargin: 5,
+	      prevText: "",          
+		  nextText: "",
+	      minItems: getGridSize(), // use function to pull in initial value
+	      maxItems: getGridSize() // use function to pull in initial value
+	    });
+	  });
+	 
+	  // check grid size on resize event
+	  $window.resize(function() {
+	    var gridSize = getGridSize();
+	 
+	    flexslider.vars.minItems = gridSize;
+	    flexslider.vars.maxItems = gridSize;
+	  });
+	}());
 	
 	//============================== Tooltip =========================
 	 
