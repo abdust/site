77e7bf5d67fd3d31a3901f51854ad78624033f45
Abdus Salam <iamabdus@gmail.com>
2016-05-28 19:04:02 +0600

    eden 1.0 update

--- eden/1.0/js/custom.js
+++ eden/1.0/js/custom.js
@@ -177,3 +177,20 @@ jQuery(document).ready(function() {
 jQuery(document).ready(function() {
 	$('#input-id').rating('update', 1);
 });
+
+//================================ zopim live chat ==================================
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?OsxjyZjGcD9KEceHsAC5toZnWGB0LVTM";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+
+//================================ Google Analytics ==================================
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-71155940-2', 'auto');
+ga('send', 'pageview');
