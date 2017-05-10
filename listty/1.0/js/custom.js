a92527060bfc4fbb43eb212e4eee7da80b52bfd1
Abdus Salam <iamabdus@gmail.com>
2017-05-10 20:49:40 +0600

    list 1.0 update

--- listty/1.0/js/custom.js
+++ listty/1.0/js/custom.js
@@ -1,5 +1,40 @@
 jQuery(document).ready(function(){
   'use strict';
+
+  //============================== MENU SCROLL =========================
+   $(window).load(function(){
+    $('.body-wrapper').each(function(){
+      var header_area = $('.header');
+      var main_area = header_area.children('.nav-wrapper');
+
+      var logo = main_area.find('.navbar-header');
+      var navigation = main_area.find('.navbar-collapse');
+      var original = {
+        nav_top: navigation.css('margin-top')
+      };
+
+      $(window).scroll(function(){
+        if( main_area.hasClass('bb-fixed-header') && ($(this).scrollTop() === 0 || $(this).width() < 750)){
+          main_area.removeClass('bb-fixed-header').appendTo(header_area);
+          navigation.animate({'margin-top': original.nav_top}, {duration: 300, queue: false, complete: function(){
+            header_area.css('height', 'auto');
+          }});
+        }else if( !main_area.hasClass('bb-fixed-header') && $(this).width() > 750 &&
+          $(this).scrollTop() > header_area.offset().top + header_area.height() - parseInt($('html').css('margin-top'), 10) ){
+
+          header_area.css('height', header_area.height());
+          main_area.css({'opacity': '0'}).addClass('bb-fixed-header');
+          main_area.appendTo($('body')).animate({'opacity': 1});
+
+          navigation.css({'margin-top': '0px'});
+        }
+      });
+    });
+
+    $(window).trigger('resize');
+    $(window).trigger('scroll');
+  });
+
 //============================== PRE LOADER =============================================
   $(window).load(function() {
     $(".page-loader").fadeOut();
@@ -7,7 +42,7 @@ jQuery(document).ready(function(){
 
 //============================== SELECT BOX =========================
   $('.select-drop').selectbox();
-  
+
 //============================== HEADER =========================
 
   $('.navbar a.dropdown-toggle').on('click', function(e) {
@@ -16,22 +51,21 @@ jQuery(document).ready(function(){
           var li = $(this).parent();
           var heightParent = parseInt(elmnt.css('height').replace('px', '')) / 2;
           var widthParent = parseInt(elmnt.css('width').replace('px', '')) - 10;
-          
+
           if(!li.hasClass('open')){
             li.addClass('open');
-          } 
+          }
           else {
             li.removeClass('open');
             $(this).next().css('top', heightParent + 'px');
             $(this).next().css('left', widthParent + 'px');
-          } 
-          
+          }
+
           return false;
       }
   });
 
   //============================== ALL DROPDOWN ON HOVER =========================
-  jQuery(document).ready(function(){
     if($('.navbar').width() > 1007)
     {
       $('.nav .dropdown').hover(function() {
@@ -41,10 +75,9 @@ jQuery(document).ready(function(){
             $(this).removeClass('open');
         });
     }
-  });
 
 //============================== SMOOTH SCROLLING TO SECTION =========================
-$(document).ready(function () {
+
   $('.scrolling  a[href*="#"]').on('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
@@ -56,6 +89,13 @@ $(document).ready(function () {
       mobileHA: false
     });
   });
+
+// scroll to a div with the ID "scrollToThis" by clicking a link with the class "scrollLink"
+$('.scrolling').click( function() {
+     $('html, body').animate({
+          scrollTop: $('#categories').offset().top -50,
+          scrollTop: $('#message').offset().top -50
+     }, 600);
 });
 
 //============================== BOOTSTRA THUMBNAIL SLIDER =========================
@@ -109,32 +149,28 @@ $(document).ready(function () {
   }());
 
 //============================== COUNTER-UP =========================
-  $(document).ready(function ($) {
     $('.counter').counterUp({
       delay: 10,
       time: 2000
     });
-  });
 
 //============================== DATE-PICKER =========================
-$(document).ready(function () {
+
   $('.datepicker').datepicker({
     startDate: 'dateToday',
     autoclose: true
   });
-});
+
 //============================== FILE UPLOADER =========================
 $(document).on('click', '.browse', function(){
   var file = $(this).parent().parent().parent().find('.file');
   file.trigger('click');
 });
 //============================== DATA TABLE =========================
-$(document).ready(function() {
-    $('#ordersTable').DataTable();
-});
 
-//============================== MAIN SLIDER ========================= 
- 
+
+//============================== MAIN SLIDER =========================
+
   var $heroSlider = $( '.main-slider .inner' );
   if ( $heroSlider.length > 0 ) {
     $heroSlider.each( function () {
@@ -157,7 +193,7 @@ $(document).ready(function() {
         rtl:false
       });
     });
-  } 
+  }
 
 //============================== OWL-CAROUSEL =========================
 
@@ -188,19 +224,18 @@ var owl = $('.owl-carousel.partnersLogoSlider');
     }
   });
 
+
 //============================== FANCY BOX =========================
-  $(document).ready(function() {
-    
+
     $('a.group').fancybox({
       'transitionIn'  : 'elastic',
       'transitionOut' : 'elastic',
-      'speedIn'   : 600, 
-      'speedOut'    : 200, 
+      'speedIn'   : 600,
+      'speedOut'    : 200,
       'overlayShow' : false
     });
-    
-  });
-  
+
+
 //============================== CLOSE BUTTON =========================
   $('.close-btn').click(function () {
     $(this).parent().hide();
--- listty/1.0/js/map.js
+++ listty/1.0/js/map.js
@@ -1,373 +1,261 @@
-/*
- * 5 ways to customize the infowindow
- * 2015 - en.marnoto.com
-*/
+// Create custom map style here: https://mapstyle.withgoogle.com/
+
+var mapStyles = [{
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#f5f5f5'
+    }]
+},
+{
+    'elementType': 'labels.icon',
+    'stylers': [{
+        'visibility': 'off'
+    }]
+},
+{
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#616161'
+    }]
+},
+{
+    'elementType': 'labels.text.stroke',
+    'stylers': [{
+        'color': '#f5f5f5'
+    }]
+},
+{
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#bdbdbd'
+    }]
+},
+{
+    'featureType': 'poi',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#eeeeee'
+    }]
+},
+{
+    'featureType': 'poi',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#757575'
+    }]
+},
+{
+    'featureType': 'poi.park',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#e5e5e5'
+    }]
+},
+{
+    'featureType': 'poi.park',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#9e9e9e'
+    }]
+},
+{
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#ffffff'
+    }]
+},
+{
+    'featureType': 'road.arterial',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#757575'
+    }]
+},
+{
+    'featureType': 'road.highway',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#dadada'
+    }]
+},
+{
+    'featureType': 'road.highway',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#616161'
+    }]
+},
+{
+    'featureType': 'road.local',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#9e9e9e'
+    }]
+},
+{
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#e5e5e5'
+    }]
+},
+{
+    'featureType': 'transit.station',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#eeeeee'
+    }]
+},
+{
+    'featureType': 'water',
+    'elementType': 'geometry',
+    'stylers': [{
+        'color': '#c9c9c9'
+    }]
+},
+{
+    'featureType': 'water',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#9e9e9e'
+    }]
+}
+];
 
 // map center
 var center = new google.maps.LatLng(-33.91722, 151.23064);
 
-// marker position
-var factory = new google.maps.LatLng(-33.91721, 151.22630);
-
-var creatures = [{
-     position: new google.maps.LatLng(-33.91721, 151.22630),
-     type: 'info',
-     markerImage:'active.png',
-     id:1,
-     is_logged_in:1
- }, {
-     position: new google.maps.LatLng(-33.91539, 151.22820),
-     type: 'info',
-     markerImage:'active.png',
-     id:2,
-     is_logged_in:1
- }, /*{
-     position: new google.maps.LatLng(-33.91747, 151.22912),
-     type: 'info',
-     markerImage:'inactive.png',
-     id:3,
-     is_logged_in:0
- },*/
-  {
-     position: new google.maps.LatLng(-33.91848, 151.2291),
-     type: 'info',
-     markerImage:'inactive.png',
-     id:4,
-     is_logged_in:0
- },
- {
-     position: new google.maps.LatLng(-33.91947, 151.23312),
-     type: 'info',
-     markerImage:'inactive.png',
-     id:5,
-     is_logged_in:0
- },
- {
-     position: new google.maps.LatLng(-33.91356, 151.22912),
-     type: 'info',
-     markerImage:'inactive.png',
-     id:6,
-     is_logged_in:0
- }
- ];
-
-// map color style
-  var mapStyles = [{
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#f5f5f5"
-         }]
-     },
-     {
-         "elementType": "labels.icon",
-         "stylers": [{
-             "visibility": "off"
-         }]
-     },
-     {
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#616161"
-         }]
-     },
-     {
-         "elementType": "labels.text.stroke",
-         "stylers": [{
-             "color": "#f5f5f5"
-         }]
-     },
-     {
-         "featureType": "administrative.land_parcel",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#bdbdbd"
-         }]
-     },
-     {
-         "featureType": "poi",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#eeeeee"
-         }]
-     },
-     {
-         "featureType": "poi",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#757575"
-         }]
-     },
-     {
-         "featureType": "poi.park",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#e5e5e5"
-         }]
-     },
-     {
-         "featureType": "poi.park",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#9e9e9e"
-         }]
-     },
-     {
-         "featureType": "road",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#ffffff"
-         }]
-     },
-     {
-         "featureType": "road.arterial",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#757575"
-         }]
-     },
-     {
-         "featureType": "road.highway",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#dadada"
-         }]
-     },
-     {
-         "featureType": "road.highway",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#616161"
-         }]
-     },
-     {
-         "featureType": "road.local",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#9e9e9e"
-         }]
-     },
-     {
-         "featureType": "transit.line",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#e5e5e5"
-         }]
-     },
-     {
-         "featureType": "transit.station",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#eeeeee"
-         }]
-     },
-     {
-         "featureType": "water",
-         "elementType": "geometry",
-         "stylers": [{
-             "color": "#c9c9c9"
-         }]
-     },
-     {
-         "featureType": "water",
-         "elementType": "labels.text.fill",
-         "stylers": [{
-             "color": "#9e9e9e"
-         }]
-     }
- ];
-
+//Map initialize function
 function initialize() {
-  var mapOptions = {
-    center: center,
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles:mapStyles,
-    scrollwheel: false
-  };
+    var mapOptions = {
+        center: center,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles:mapStyles,
+        scrollwheel: false
+    };
+    //create a google map instance into the Dom element
+    var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+    /*
+    Path to json file that contains listing data for the marker. Make sure you are calling this file through a server.
+     */
+    var url = 'js/listings.json';
 
-  // InfoWindow content
-      var content = '<div id="iw-container">' +
-                    '<div class="iw-content">' +
-                      '<ul class="list-inline rating">'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                      '</ul>'+
-                      '<div class="iw-subTitle">The City Theater</div>' +
-                      '<p>10 Bay Street Toronto</p>'+
-                      '<p>Ontario Canada</p>'+
-                    '</div>' +
-                    '<div class="iw-bottom-gradient"></div>' +
-                  '</div>';
-
-  
-
-  // *
-  // START INFOWINDOW CUSTOMIZE.
-  // The google.maps.event.addListener() event expects
-  // the creation of the infowindow HTML structure 'domready'
-  // and before the opening of the infowindow, defined styles are applied.
-  // *
- 
-
-  //custom code
-
-  function getMarkerContent(creature){
-    var content=' <div class="flip-container">'+
-                    '<div class="flipper">'+
-                        '<div class="front">'+
-                            '<img src="img/map/marker-front.png">'+
-                        '</div>'+
-                        '<div class="back">'+
-                           '<img src="img/map/marker-back.png">'+
-                        '</div>'+
-                    '</div>'+
-                '</div>';
-    return content;             
-  }
-
-
-   function addMarker(creature){
-      var markerContent=getMarkerContent(creature);
-      var marker = new RichMarker({
-            position: creature.position,
-            map: map,
-            content:markerContent,
-            title:"Fábrica de Porcelana da Vista Alegre",
-            is_logged_in:creature.is_logged_in
-      });
-
-      // InfoWindow content
-      var content = '<div id="iw-container">' +
-                    '<div class="iw-content">' +
-                      '<ul class="list-inline rating">'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                        '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
-                      '</ul>'+
-                      '<div class="iw-subTitle">The City Theater</div>' +
-                      '<p>10 Bay Street Toronto</p>'+
-                      '<p>Ontario Canada</p>'+
-                    '</div>' +
-                    '<div class="iw-bottom-gradient"></div>' +
-                  '</div>';
-
-      // A new Info Window is created and set content
-      var infowindow = new google.maps.InfoWindow({
-          content: content,
-          maxWidth: 270
-      });
-   
-
-      // This event expects a click on a marker
-      // When this event is fired the Info Window is opened.
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-      });
-
-      // Event that closes the Info Window with a click on the map
-      /*google.maps.event.addListener(map, 'click', function() {
-        infowindow.close();
-      });*/
-
-      // *
-      // START INFOWINDOW CUSTOMIZE.
-      // The google.maps.event.addListener() event expects
-      // the creation of the infowindow HTML structure 'domready'
-      // and before the opening of the infowindow, defined styles are applied.
-      // *
-
-      google.maps.event.addListener(infowindow, 'domready', function() {
-        // Reference to the DIV that wraps the bottom of infowindow
-        var iwOuter = $('.gm-style-iw');
-
-        /* Since this div is in a position prior to .gm-div style-iw.
-         * We use jQuery and create a iwBackground variable,
-         * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
-        */
-        var iwBackground = iwOuter.prev();
-
-        // Removes background shadow DIV
-        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
-        // Removes white background DIV
-        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-
-        // Moves the infowindow 115px to the right.
-        iwOuter.parent().parent().css({left: '115px'});
-
-        // iwOuter.parent().parent().children().find('div').css({left: '115px'});
-        // Moves the shadow of the arrow 76px to the left margin.
-        iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-
-        // Moves the arrow 76px to the left margin.
-        iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-
-        // Changes the desired tail shadow color.
-        iwOuter.parent().css({'width' : '270px'});
-        iwOuter.children(':first-child').css({'width' : '100%', 'height' : '182px'});
-        iwOuter.parent().children(':first-child').find('div').css({'display' : 'none'});
-
-        // Changes the desired tail shadow color.
-        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1', 'display' : 'none'});
-
-        // Reference to the div that groups the close button elements.
-        var iwCloseBtn = iwOuter.next();
-
-        // Apply the desired effect to the close button
-        iwCloseBtn.css({width: '26px', height:'27px', opacity: '1', right: '8px', top: '-35px', border: '7px solid #ffffff', 'border-radius': '13px', 'box-shadow': '0 0 5px rgba(0, 0, 0, .2)'});
-
-        // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
-        if($('.iw-content').height() < 140){
-          $('.iw-bottom-gradient').css({display: 'none'});
-        }
-        // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
-        iwCloseBtn.mouseout(function(){
-            $(this).css({opacity: '1'});
+    // define the format of the file retrive from server. here it is in JSON format
+    var mapdata = {
+        format: 'json'
+    };
+    // the ajax callback function. Do all the stuff you want to do with the remote data in between this function.
+    function getContent(data) {
+        //loop through each of the single JSON object obtained from the JSON file.
+        var markers = [];
+        var infobox = new InfoBox({
+            content: 'empty',
+            disableAutoPan: false,
+            maxWidth: 0,
+            pixelOffset: new google.maps.Size(-250, -330),
+            zIndex: null,
+            closeBoxURL: "",
+            infoBoxClearance: new google.maps.Size(1, 1),
+            isHidden: false,
+            isOpen: false,
+            pane: "floatPane",
+            enableEventPropagation: false
         });
-    });
+        infobox.addListener('domready', function () {
+            $('.infobox-close').on('click', function () {
+                infobox.close(map, this);
+                infobox.isOpen = false;
+            });
+        });
+        $.each(data, function (i, value) {
+            var markerCenter = new google.maps.LatLng(value.latitude, value.longitude);
 
-    return marker;
-
-  }
-
-  var allMarkerId={};
-  for (var i=0;i<creatures.length;i++) {
-       allMarkerId[creatures[i].id]=addMarker(creatures[i]);
-  };
-
-  
-
-  function updateMarker(marker){
-      if(marker.is_logged_in==0){
-        return false;
-      }
-      var random=Math.random() * ((.0002-.0001)+.0001);
-      var lat=marker.getPosition().lat()+random;
-      var lng=marker.getPosition().lng()+random;
-      var newPostion=new google.maps.LatLng(lat, lng);
-      var oldPosition=marker.getPosition();
-   
-      marker.setPosition(newPostion);
+            var verified = '';
 
 
-     /*console.log(marker.getPosition().lat());*/
-  }
-
-  // setInterval(function(){
-  //    for(var i=0;i<creatures.length;i++){
-  //       //allMarkerId[creatures[i].id].setVisible(false);
-  //       updateMarker(allMarkerId[creatures[i].id]);
-  //    }
-  // },3000)
+            if (value.verified) {
+                verified = '<div class="marker-verified"><i class="fa fa-check"></i></div>';
+            }
 
 
+            function getMarkerContent(value){
+                var content='<div id="marker-'+ value.id +'" class="flip-container">'+ verified +
+                '<div class="flipper">'+
+                '<div class="front">'+
+                '<img src="'+value.thumbnail+'">'+
+                '</div>'+
+                '<div class="back">'+
+                '<i class="fa fa-eye"></i>'+
+                '</div>'+
+                '</div>'+
+                '</div>';
+                return content;
+            }
 
+            var markerContent=getMarkerContent(value);
+            var marker = new RichMarker({
+                id: value.id,
+                data: value,
+                flat: true,
+                position: markerCenter,
+                map: map,
+                shadow: 0,
+                content:markerContent,
+                title:"Fábrica de Porcelana da Vista Alegre",
+                is_logged_in:value.is_logged_in
+            });
+            markers.push(marker);
+
+            // This event expects a click on a marker
+            // When this event is fired the Info Window is opened.
+            google.maps.event.addListener(marker, 'click', function() {
+
+                var content = '<div class="infobox-close"><i class="fa fa-close"></i></div>'+
+                '<div id="iw-container" style="background-image: url(' + marker.data.thumbnail + ');">' +
+                '<div class="iw-content">' +
+                '<ul class="list-inline rating">'+
+                '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
+                '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
+                '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
+                '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
+                '<li><i class="fa fa-star" aria-hidden="true"></i></li>'+
+                '</ul>'+
+                '<div class="iw-subTitle">'+ marker.data.title +'</div>' +
+                '<p>' + marker.data.address + '</p>'+
+                '</div>' +
+                '<div class="iw-bottom-gradient"></div>' +
+                '</div>';
+
+                if (!infobox.isOpen) {
+                    infobox.setContent(content);
+                    infobox.open(map, this);
+                    infobox.isOpen = true;
+                    infobox.markerId = marker.id;
+                } else {
+                    if (infobox.markerId == marker.id) {
+                        infobox.close(map, this);
+                        infobox.isOpen = false;
+                    } else {
+                        infobox.close(map, this);
+                        infobox.isOpen = false;
+
+                        infobox.setContent(content);
+                        infobox.open(map, this);
+                        infobox.isOpen = true;
+                        infobox.markerId = marker.id;
+                    }
+                }
+            });
+        })
+    }
+    // call the jquery ajax function
+    $.getJSON(url, mapdata, getContent);
+} // map initialize function ends
+var existId = document.getElementById("map-canvas");
+if (existId) {
+    google.maps.event.addDomListener(window, 'load', initialize);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
