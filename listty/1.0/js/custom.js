e8aec34bf625d5be1d49caaf8d4ec197fb9c61c4
Abdus Salam <iamabdus@gmail.com>
2017-05-23 10:37:30 +0600

    kidz 1.2

--- listty/1.0/js/custom.js
+++ listty/1.0/js/custom.js
@@ -148,6 +148,22 @@ $('.scrolling').click( function() {
     });
   }());
 
+  //============================== Google Analytics =========================
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71155940-7', 'auto');
+  ga('send', 'pageview');
+
+
+  //============================== getsitecontrol =========================
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/46851/script.js');
+
+
 //============================== COUNTER-UP =========================
     $('.counter').counterUp({
       delay: 10,
