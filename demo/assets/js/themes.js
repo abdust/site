72382ed91efe02f6053c6e7a2be3ab797509896d
iamabdus <iamabdus@gmail.com>
2019-01-13 12:37:51 +0600

    demo: update

---
+++ demo/assets/js/themes.js
@@ -0,0 +1,122 @@
+var $themes, $defaultTheme = 'kidzPro_html';
+
+var imgURL = 'assets/img/themes/';
+
+
+// Products List
+// ---------------------------------------------------------
+$themes = {
+	kidzPro_html: {
+		name      : 'Kidz Pro',
+		tag       : 'BS4',
+		img       : 'kidz.png',
+		url       : 'https://themes.iamabdus.com/kidz/2.0/',
+		purchase  : 'https://gum.co/kidzpro',
+		tooltip   : 'Multipurpose Children Kids Theme'
+	},
+	sleek_html: {
+    name      : 'Sleek Dashboard',
+    tag       : 'BS4',
+    img       : 'sleek-html.png',
+    url       : 'https://sleek.tafcoder.com/',
+    purchase  : 'https://github.com/tafcoder/sleek-dashboard',
+    tooltip   : 'Free Dashboard Admin Template'
+  },
+	onex_html: {
+    name      : 'Onex',
+    tag       : 'BS4',
+    img       : 'onex.png',
+    url       : 'https://themes.iamabdus.com/onex/1.1/app/',
+    purchase  : 'http://bit.ly/WB-Onex',
+    tooltip   : 'Multipurpose Landing Page Templates'
+  },
+	wedex_html: {
+		name      : 'Wedex',
+		tag       : 'BS4',
+		img       : 'wedex.png',
+		url       : 'https://themes.iamabdus.com/wedex/1.0/',
+		purchase  : 'http://bit.ly/WB-Wedex',
+		tooltip   : 'Lovely & Elegant Wedding Theme'
+	},
+	corporex_html: {
+		name      : 'Corporex',
+		tag       : 'BS4',
+		img       : 'corporex.png',
+		url       : 'https://themes.iamabdus.com/corporex/1.1/',
+		purchase  : 'http://bit.ly/WB-Corporex',
+		tooltip   : 'Corporate and Business Theme'
+	},
+	nexa_html: {
+    name      : 'Nexa',
+    tag       : 'BS4',
+    img       : 'nexa.png',
+    url       : 'https://themes.iamabdus.com/nexa/1.2/',
+    purchase  : 'http://bit.ly/WB-Nexa',
+    tooltip   : 'Creative Agency Portfolio Theme'
+  },
+	biznex_html: {
+    name      : 'Biznex',
+    tag       : 'BS4',
+    img       : 'biznex.png',
+    url       : 'https://themes.iamabdus.com/biznex/1.2/',
+    purchase  : 'http://bit.ly/WB-Biznex',
+    tooltip   : 'Multipurpose Business Theme'
+  },
+	adora_html: {
+		name      : 'Adora',
+		tag       : 'BS4',
+		img       : 'adora.png',
+		url       : 'https://themes.iamabdus.com/adora/1.0/',
+		purchase  : 'http://bit.ly/WB-Adora',
+		tooltip   : 'Beauty Spa & Massage Salon Theme'
+	},
+	listty_html: {
+		name      : 'Listty',
+		tag       : 'BS',
+		img       : 'listty.png',
+		url       : 'https://themes.iamabdus.com/listty/1.4/index-2.html',
+		purchase  : 'http://bit.ly/WB-Listty',
+		tooltip   : 'Directory Listing + Admin Theme'
+	},
+	trust_html: {
+		name      : 'Trust',
+		tag       : 'BS',
+		img       : 'trust.png',
+		url       : 'https://themes.iamabdus.com/trust/1.1/',
+		purchase  : 'http://bit.ly/WB-Trust',
+		tooltip   : 'Modern Lawyer Attorney Theme'
+	},
+	kidz_html: {
+    name      : 'Kidz',
+    tag       : 'BS',
+    img       : 'kidz.png',
+    url       : 'https://themes.iamabdus.com/kidz/2.0/',
+    purchase  : 'http://bit.ly/WB-Kidz',
+    tooltip   : 'Multipurpose Children Kids Theme'
+  },
+	star_html: {
+		name      : 'Star',
+		tag       : 'BS',
+		img       : 'star.png',
+		url       : 'https://themes.iamabdus.com/star/1.3/',
+		purchase  : 'http://bit.ly/WB-Star',
+		tooltip   : 'Tour Booking Theme + Admin'
+	},
+  bigbag_html: {
+    name      : 'BigBag',
+    tag       : 'BS4',
+    img       : 'bigbag.png',
+    url       : 'https://themes.iamabdus.com/bigbag/2.1/',
+    purchase  : 'http://bit.ly/WB-Bigbag',
+    tooltip   : 'Elegant Ecommerce Theme'
+  },
+	royal_html: {
+		name      : 'Royal',
+		tag       : 'BS',
+		img       : 'royal.png',
+		url       : 'https://themes.iamabdus.com/royal/1.3/',
+		purchase  : 'http://bit.ly/WB-Royal',
+		tooltip   : 'Multipurpose Education Template'
+	}
+
+};
