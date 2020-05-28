72bf02501b8973f9f50fb6a27ae2c251dbbc654b
Abdus Salam <iamabdus@gmail.com>
2020-05-29 03:37:11 +0600

    bigbag 2.2 demo

--- demo/assets/js/themes.js
+++ demo/assets/js/themes.js
@@ -106,7 +106,7 @@ $themes = {
         name      : 'BigBag',
         tag       : 'BS4',
         img       : 'bigbag.png',
-        url       : 'https://themes.iamabdus.com/bigbag/2.1/',
+        url       : 'https://themes.iamabdus.com/bigbag/2.2/',
         purchase  : 'http://bit.ly/WB-Bigbag',
         tooltip   : 'Elegant Ecommerce Theme'
     },
